97f580d8c6eec7d57245c4243122d82fd1a9675f
Ralph Carandang <rvncrndng@gmail.com>
2025-07-17 17:24:47 +0800

    Update axios config

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.MODE == 'development' ? 'http://localhost:5001/api' : 'https://ralph-reeven.vercel.app/api'
+const BASE_URL = import.meta.env.MODE == 'development' ? 'http://localhost:5001/api' : 'https://personal-fortfolio-backend.onrender.com'
 const instance = axios.create({
     baseURL: BASE_URL
 })
