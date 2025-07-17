8453f084f0c078fa2fdef99b6e558af71b91ea95
arar <109428773+ralphreevencarandang@users.noreply.github.com>
2025-07-17 17:18:57 +0800

    Update axios.js base url

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const BASE_URL = import.meta.env.MODE == 'development' ? 'http://localhost:5001/api' : 'https://ralph-reeven.vercel.app/api'
 const instance = axios.create({
-    baseURL: BASE_URL
+    baseURL: "https://ralph-reeven.vercel.app/api"
 })
 
 export default instance
